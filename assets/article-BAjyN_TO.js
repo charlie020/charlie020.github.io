2bac574c865e56398ec90c624ec99dc417f23a97
Endymion <2580390720@qq.com>
2025-09-17 17:13:49 +0800

    feat: blog auto

---
+++ assets/article-BAjyN_TO.js
@@ -0,0 +1,15 @@
+const r=`---\r
+title: '我的第一篇日记'\r
+date: '2025-09-16'\r
+tags: ['生活', '编码', 'Vue']\r
+snippet: '1231245'\r
+---\r
+\r
+# Vue 学习之旅的开始\r
+\r
+今天，我正式开始了我的 Vue 博客搭建之旅。过程虽然充满挑战，但也收获满满。\r
+\r
+## 遇到的问题\r
+\r
+最大的问题是如何优雅地处理 Markdown。我发现使用 \`markdown-it\` 和 \`front-matter\` 这两个库可以完美解决问题。\r
+`;export{r as default};

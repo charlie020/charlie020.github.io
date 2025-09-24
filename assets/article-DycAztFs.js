3d26d50d067d235ddc02f3d7e6ec956fc337e942
Endymion <2580390720@qq.com>
2025-09-24 22:31:58 +0800

    feat: git和航展博客初步

---
+++ assets/article-DycAztFs.js
@@ -0,0 +1,26 @@
+const n=`---\r
+title: '长春航空展'\r
+date: '2025-09-23'\r
+tags: ['生活', '摄影']\r
+snippet: '关于2025年长春航空展的一些摄影日记'\r
+imageUrl: '/articles/life/2025-09-23/1.jpg'\r
+---\r
+\r
+## 出发\r
+早晨6:20\r
+![](/articles/life/2025-09-23/2.jpg){.image-center}\r
+\r
+## 进场\r
+8:00准时进场\r
+\r
+## 表演\r
+9:00伞兵跳伞\r
+\r
+10:20飞行表演\r
+\r
+## 飞机静态展示\r
+下午13:20，饭后\r
+\r
+\r
+<br/>\r
+<span style="font-size: 30px; font-weight: bold;">END</span>`;export{n as default};

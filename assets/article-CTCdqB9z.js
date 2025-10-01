2e73494b088da9a79d08bd9d1199f7b7c858de1d
Charlie020 <2580390720@qq.com>
2025-10-01 14:21:30 +0800

    航展

---
+++ assets/article-CTCdqB9z.js
@@ -0,0 +1,113 @@
+const n=`---
+title: '长春航空展'
+date: '2025-09-23'
+tags: ['生活', '摄影']
+snippet: '关于2025年长春航空展的一些摄影日记'
+imageUrl: '/articles/life/2025-09-23/1.jpg'
+---
+
+## 出发
+早晨6:00起床坐3号线地铁跟女朋友在卫星广场汇合，随后坐1号线到人民广场，乘坐航展直达车79路公交。
+
+刚出人民广场地铁站，就看到一长列公交车（有79路和其他路公交）停在路旁等待着乘客上车，看样子长春为今年的航展做了大量的协调工作。
+
+工作人员拿着大喇叭指挥着人流，排队的队伍在工作人员的协调下分成了两队，分别代表着有座和无座。
+
+经过5分钟的有座排队后，我们上了车。在摇摇晃晃的公交上，我们昏昏沉沉的睡了大约40分钟后，8:00准时到达了长春航空博物馆A1检票口。
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/a1检票口.jpg">
+  <em>长春航空展A1检票口</em>
+</div>
+
+## 进场
+8:00准时进场，进来直走200米就看到了一排的战机静态展示，武器迷狂喜😁
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/引擎口.jpg">
+  <em>(应该是)苏27的引擎口也太大了</em>
+</div>
+
+来不及看静态展示了，得赶紧去抢跑道
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/表演前排.jpg">
+  <em>跑道前排往后望，人太多了</em>
+</div>
+
+终于到前排来了
+
+## 表演
+9:00伞兵跳伞
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/跳伞.jpg">
+  <em>跳伞</em>
+</div>
+
+10:20飞行表演
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/螺旋桨飞机拉白烟.jpg">
+  <em>拉烟1</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/螺旋桨飞机拉红烟.jpg">
+  <em>拉烟2</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/螺旋桨飞机拉红烟2.jpg">
+  <em>拉烟3</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/螺旋桨飞机拉橙烟.jpg">
+  <em>拉烟4</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/螺旋桨飞机拉彩烟.jpg">
+  <em>拉烟5</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/f16跑道.jpg">
+  <em>f16跑道</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/f16飞行.jpg">
+  <em>f16飞行</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/运20飞行.jpg">
+  <em>运20</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/歼20飞行.jpg">
+  <em>歼20</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/81-4架拉彩烟.jpg">
+  <em>八一表演队1</em>
+</div>
+
+<div class="figure-center">
+  <img src="/articles/life/2025-09-23/81-6架拉彩烟.jpg">
+  <em>八一表演队2</em>
+</div>
+
+
+## 飞机静态展示
+12:20表演结束，去C餐厅排了20分钟队点了一碗知北村酸辣粉，15块钱还挺便宜。
+
+下午13:30，饭后开始了下午场表演，但下午的飞机没有上午先进。
+
+
+<br/>
+<span style="font-size: 30px; font-weight: bold;">END</span>`;export{n as default};
